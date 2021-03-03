a33ccfc41d2cf2f002fdc393e9675d46c321146d
jangsunwoo <47623096+jangsunwoo@users.noreply.github.com>
2021-03-03 17:30:47 +0900

    2021-03-03

---
+++ Assets/Scripts/Statics/GameStatics.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+// 게임 제작에 필요한 정적 필드, 정적 메서드를 제공할 클래스
+public static class GameStatics
+{
+	// 화면 크기에 대한 읽기 전용 프로퍼티
+	public static (float width, float height) screenSize = (1600.0f, 900.0f);
+
+	// 화면 비율에 대한 읽기 전용 프로퍼티
+	public static float screenRatio => Screen.width / screenSize.width;
+
+}
---
+++ Assets/Scripts/UI/VirtualJoystick.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public sealed class VirtualJoystick : MonoBehaviour,
+	IDragHandler, IPointerDownHandler, IPointerUpHandler
+/// - IDragHandler : 드래깅 콜백을 받기 위해 구현해야 하는 인터페이스
+{
+	// 사용되는 이미지 컴포넌트
+	[Header("Joystick")]
+	[SerializeField] private Image _JoystickThumbImage;
+
+	// 조이스틱 입력중을 나타냅니다.
+	public bool isInput { get; private set; }
+
+	public RectTransform rectTransform => transform as RectTransform;
+
+	void IDragHandler.OnDrag(PointerEventData eventData)
+	{
+		// 입력된 위치를 저장합니다.
+		Vector2 inputPos = eventData.position / GameStatics.screenRatio;
+
+		// 입력된 위치에서 배경 위치를 뺍니다.
+		inputPos -= rectTransform.anchoredPosition;
+
+
+		// 조이스틱을 가둡니다.
+		inputPos = (Vector2.Distance(Vector2.zero, inputPos) < rectTransform.sizeDelta.x * 0.5f) ?
+			inputPos : inputPos.normalized * rectTransform.sizeDelta.x * 0.5f;
+
+		// 조이스틱 위치를 설정합니다.
+		_JoystickThumbImage.rectTransform.anchoredPosition = inputPos;
+	}
+
+	void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+	{
+		isInput = true;
+		(this as IDragHandler).OnDrag(eventData);
+	}
+
+	void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
+	{
+		isInput = false;
+
+		// 조이스틱 위치를 되돌립니다.
+		_JoystickThumbImage.rectTransform.anchoredPosition = Vector2.zero;
+	}
+}
