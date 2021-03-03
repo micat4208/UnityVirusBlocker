3beb2d00e4f40b3bb7897ec0f64846a04756daab
jangsunwoo <47623096+jangsunwoo@users.noreply.github.com>
2021-03-03 15:21:44 +0900

    2021-03-03

---
+++ Assets/Scripts/CustomEditor/EditorMenuCreateSceneInstance.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+// SceneInstance 오브젝트를 생성하는 스크립트
+public sealed class EditorMenuCreateSceneInstance : MonoBehaviour
+{
+#if UNITY_EDITOR
+    [MenuItem("GameObject/Create SceneInstance", false, 10)]
+    private static void CreateSceneInstance()
+    {
+        GameObject newSceneInstanceObj = new GameObject();
+        newSceneInstanceObj.name = "SceneInstance";
+        newSceneInstanceObj.AddComponent<SceneInstance>();
+#endif
+    }
+}
